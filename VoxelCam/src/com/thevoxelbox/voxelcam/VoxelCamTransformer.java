33974891d9e77fd5b95a4eba5c54eeae03570907
thatapplefreak <thatapplefreak@gmail.com>
2013-11-10 15:04:13 -0500

    Implemented Liteloader Transformer

---
+++ VoxelCam/src/com/thevoxelbox/voxelcam/VoxelCamTransformer.java
@@ -0,0 +1,12 @@
+package com.thevoxelbox.voxelcam;
+
+import net.minecraft.launchwrapper.IClassTransformer;
+
+public class VoxelCamTransformer implements IClassTransformer {
+
+	@Override
+	public byte[] transform(String arg0, String arg1, byte[] arg2) {
+		return null;
+	}
+
+}
