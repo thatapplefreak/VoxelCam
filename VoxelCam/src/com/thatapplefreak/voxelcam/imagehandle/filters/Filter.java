8d0725fa569843d7c93da1d53f1d0fc85bcf6b08
Byron Zaharako <thatapplefreak@gmail.com>
2014-01-22 13:42:14 -0500

    Convert Filter from abstract class to interface

--- VoxelCam/src/com/thatapplefreak/voxelcam/imagehandle/filters/Filter.java
+++ VoxelCam/src/com/thatapplefreak/voxelcam/imagehandle/filters/Filter.java
@@ -2,18 +2,8 @@ package com.thatapplefreak.voxelcam.imagehandle.filters;
 
 import java.awt.image.BufferedImage;
 
-public abstract class Filter {
+public interface Filter {
 	
-	private final BufferedImage image;
-	
-	public Filter(BufferedImage image) {
-		this.image = image;
-	}
-	
-	public BufferedImage filterImage() {
-		return this.getFilteredImage(this.image);
-	}
-
-	protected abstract BufferedImage getFilteredImage(BufferedImage img);
+	public abstract BufferedImage getFilteredImage(BufferedImage img);
 	
 }
