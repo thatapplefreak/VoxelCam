10a601ac6827abc5917350b8b2bfdcdf0fe7e5af
thatapplefreak <thatapplefreak@gmail.com>
2014-02-10 22:08:32 -0500

    Change Main menu button to less intrusive Minecraft like button

--- VoxelCam/src/com/thatapplefreak/voxelcam/gui/mainmenu/GuiMainMenuWithPhotoButton.java
+++ VoxelCam/src/com/thatapplefreak/voxelcam/gui/mainmenu/GuiMainMenuWithPhotoButton.java
@@ -1,6 +1,10 @@
 package com.thatapplefreak.voxelcam.gui.mainmenu;
 
+import org.lwjgl.opengl.GL11;
+
 import com.thatapplefreak.voxelcam.gui.manager.GuiScreenShotManager;
+import com.thevoxelbox.common.util.AbstractionLayer;
+import com.thevoxelbox.common.util.gui.AdvancedDrawGui;
 
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiMainMenu;
@@ -8,14 +12,12 @@ import net.minecraft.util.ResourceLocation;
 
 public class GuiMainMenuWithPhotoButton extends GuiMainMenu {
 
-	static ResourceLocation photoBtnPic = new ResourceLocation("voxelcam", "textures/camerabtn.png");
-
 	private PhotoButton photoBtn;
 
 	@Override
 	public void initGui() {
 		super.initGui();
-		this.photoBtn = new PhotoButton(width - 52, 5);
+		this.photoBtn = new PhotoButton(width / 2 + 104, this.height / 4 + 132);
 		buttonList.add(photoBtn);
 	}
 
@@ -26,5 +28,5 @@ public class GuiMainMenuWithPhotoButton extends GuiMainMenu {
 			mc.displayGuiScreen(new GuiScreenShotManager());
 		}
 	}
-
+	
 }
--- VoxelCam/src/com/thatapplefreak/voxelcam/gui/mainmenu/PhotoButton.java
+++ VoxelCam/src/com/thatapplefreak/voxelcam/gui/mainmenu/PhotoButton.java
@@ -1,22 +1,27 @@
 package com.thatapplefreak.voxelcam.gui.mainmenu;
 
+import org.lwjgl.opengl.GL11;
+
+import com.thevoxelbox.common.util.AbstractionLayer;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.util.ResourceLocation;
 
 public class PhotoButton extends GuiButton {
+	
+	static ResourceLocation photoBtnPic = new ResourceLocation("voxelcam", "textures/photo.png");
 
 	public PhotoButton(int xPos, int yPos) {
-		super(111195, xPos, yPos, 47, 47, "");
+		super(111195, xPos, yPos, 20, 20, "");
+		
 	}
-
+	
 	@Override
-	public void drawButton(Minecraft mc, int mouseX, int mouseY) {
-		mc.getTextureManager().bindTexture(GuiMainMenuWithPhotoButton.photoBtnPic);
-		if (mouseX >= this.field_146128_h && mouseY >= this.field_146129_i && mouseX < this.field_146128_h + this.field_146120_f && mouseY < this.field_146129_i + this.field_146121_g) {
-			drawTexturedModalRect(this.field_146128_h, this.field_146129_i, 47, 0, this.field_146120_f, this.field_146121_g); // TODO
-		} else {
-			drawTexturedModalRect(this.field_146128_h, this.field_146129_i, 0, 0, this.field_146120_f, this.field_146121_g);
-		}
+	public void drawButton(Minecraft p_146112_1_, int p_146112_2_, int p_146112_3_) {
+		super.drawButton(p_146112_1_, p_146112_2_, p_146112_3_);
+		AbstractionLayer.bindTexture(photoBtnPic);
+		drawTexturedModalRect(this.field_146128_h, this.field_146129_i, 0, 0, this.field_146120_f, this.field_146121_g);
+		// TODO Why is this not drawing for some reason?
 	}
-
 }
