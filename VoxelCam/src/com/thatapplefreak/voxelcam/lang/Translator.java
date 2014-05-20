5bb77a31f9f6eb20b9315e332080e83da3a3627a
thatapplefreak <thatapplefreak@gmail.com>
2014-05-20 13:33:32 -0400

    Translator additions

--- VoxelCam/src/com/thatapplefreak/voxelcam/lang/Translator.java
+++ VoxelCam/src/com/thatapplefreak/voxelcam/lang/Translator.java
@@ -1,9 +1,37 @@
 package com.thatapplefreak.voxelcam.lang;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Properties;
+
 public class Translator {
 	
+	private static Properties english_US = getTranslation("english_US");
+	
 	public static String translate(String key) {
+		//Find 
 		return "filler";
+	}
+	
+	private static Properties getTranslation(String fileName) {
+		File langFile = null;
+		try {
+			langFile = new File(Translator.class.getResource(fileName + ".lang").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		Properties p = new Properties();
+		try {
+			p.load(new FileInputStream(langFile));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return p;
 	}
 	
 }
