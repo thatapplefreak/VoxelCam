02bc2d18cb03eb0cb7b657defbac6b70103c6020
thatapplefreak <thatapplefreak@gmail.com>
2014-05-21 17:26:08 -0400

    Reqister spanish and sweedish + add .lang to sweedish

--- VoxelCam/src/com/thatapplefreak/voxelcam/lang/Translator.java
+++ VoxelCam/src/com/thatapplefreak/voxelcam/lang/Translator.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Properties;
 
@@ -12,13 +13,19 @@ import net.minecraft.client.Minecraft;
 public class Translator {
 	
 	private static Properties english_US = getTranslation("english_US");
+	private static Properties spanish_ES = getTranslation("spanish_ES");
+	private static Properties sweedish_SWE = getTranslation("sweedish_SWE");
 	
 	public static String translate(String key) {
 		String language = Minecraft.getMinecraft().gameSettings.language;
 		
 		if (language.equals("en_US")) {
 			return english_US.getProperty(key);
-		}		
+		} else if (language.equals("es_ES")) {
+			return spanish_ES.getProperty(key);
+		} else if (language.equals("sv_SE")) {
+			return sweedish_SWE.getProperty(key);
+		}
 		
 		//Default to US English
 		return english_US.getProperty(key);
@@ -27,7 +34,8 @@ public class Translator {
 	private static Properties getTranslation(String fileName) {
 		File langFile = null;
 		try {
-			langFile = new File(Translator.class.getResource(fileName + ".lang").toURI());
+			URI res = Translator.class.getResource(fileName + ".lang").toURI();
+			langFile = new File(res);
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
