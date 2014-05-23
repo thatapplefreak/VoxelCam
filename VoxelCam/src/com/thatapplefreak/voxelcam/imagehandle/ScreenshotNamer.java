177e4c162f96aaf345f22f09b2102ab8f80aa5c6
thatapplefreak <thatapplefreak@gmail.com>
2014-05-22 20:36:35 -0400

    Reset to old screenshot naming

--- VoxelCam/src/com/thatapplefreak/voxelcam/imagehandle/ScreenshotNamer.java
+++ VoxelCam/src/com/thatapplefreak/voxelcam/imagehandle/ScreenshotNamer.java
@@ -13,10 +13,12 @@ public abstract class ScreenshotNamer {
 
 	public static File getScreenshotName() {
 		
+		String name = new SimpleDateFormat("yyyy-MM-dd_HH.mm.ss").format(new Date());
+		
 		int var3 = 1;
 
 		while (true) {
-			File var1 = new File(VoxelCamCore.getScreenshotsDir(), (var3 == 1 ? "" : "_" + var3) + ".png");
+			File var1 = new File(VoxelCamCore.getScreenshotsDir(), name + (var3 == 1 ? "" : "_" + var3) + ".png");
 
 			if (!var1.exists()) {
 				return var1;
