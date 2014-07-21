19d303a78655f595f6111fe8866eb0da27b5e789
Mumfrey <adam@eq2.co.uk>
2014-07-21 16:43:48 +0100

    Remove unnecessary duplicate "FrameBuffer"

--- src/com/thatapplefreak/voxelcam/VoxelCamTransformer.java
+++ src/com/thatapplefreak/voxelcam/VoxelCamTransformer.java
@@ -13,14 +13,13 @@ public class VoxelCamTransformer extends EventInjectionTransformer {
 	private static final String voxelCamScreenshotMethod = "takeScreenshot";
 
 	private static final Obf classChatComponent = new Obf("net.minecraft.util.IChatComponent", "fj") {};
-	private static final Obf classFrameBuffer = new Obf("net.minecraft.client.shader.Framebuffer","bmg") {};
 	private static final Obf classScreenShotHelper = new Obf("net.minecraft.util.ScreenShotHelper", "bbp") {};
 	private static final Obf methodSaveScreenshot  = new Obf("func_148260_a", "a", "saveScreenshot") {};
 	
 	@Override
 	protected void addEvents() {
 		Event onTakeScreenshot = Event.getOrCreate("onTakeScreenshot", true);
-		MethodInfo target = new MethodInfo(classScreenShotHelper, methodSaveScreenshot, classChatComponent, File.class, Integer.TYPE, Integer.TYPE, classFrameBuffer);
+		MethodInfo target = new MethodInfo(classScreenShotHelper, methodSaveScreenshot, classChatComponent, File.class, Integer.TYPE, Integer.TYPE, Obf.FrameBuffer);
 		MethodInfo callback = new MethodInfo(coreClassName,	voxelCamScreenshotMethod);
 		
 		this.addEvent(onTakeScreenshot, target, new MethodHead()).addListener(callback);
